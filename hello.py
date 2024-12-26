c0e746b4b3ba247cdf0a71d47c86715de321e9a2
dai <dai@students.acts.in>
2024-12-26 15:38:46 +0530

    dockerfile and python file added

--- hello.py
+++ hello.py
@@ -7,5 +7,5 @@ def hello():
     return 'Welcome to kubernates!!!'
 
 if __name__ == '__main__':
-    app.run(host = '0.0.0.0', debug = True)
+    app.run(host = '0.0.0.0', port = '7000', debug = True)
 
